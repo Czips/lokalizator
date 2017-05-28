577483fd24d1f004bc4bd334e6f2532f8a203163
Czips <mikro121@gmail.com>
2017-05-28 21:01:34 +0200

    db

--- routes/index.js
+++ routes/index.js
@@ -13,12 +13,63 @@ router.get('/pagecount', function (req, res) {
   res.sendStatus(200);
 });
 
+var mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
+	mongoURLLabel = "";
+
+if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
+  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
+      mongoUser = process.env[mongoServiceName + '_USER'];
+
+  if (mongoHost && mongoPort && mongoDatabase) {
+    mongoURLLabel = mongoURL = 'mongodb://';
+    if (mongoUser && mongoPassword) {
+      mongoURL += mongoUser + ':' + mongoPassword + '@';
+    }
+    // Provide UI label that excludes user id and pw
+    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
+    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
+
+  }
+}
+
+var db = null,
+    dbDetails = new Object();
+
+var initDb = function(callback) {
+  if (mongoURL == null) return;
+
+  var mongodb = require('mongodb');
+  if (mongodb == null) return;
+
+  mongodb.connect(mongoURL, function(err, conn) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    db = conn;
+    dbDetails.databaseName = db.databaseName;
+    dbDetails.url = mongoURLLabel;
+    dbDetails.type = 'MongoDB';
+
+    console.log('Connected to MongoDB at: %s', mongoURL);
+});
+}
+
 router.post('/get', function(req,res,next){
 	console.log('hejj');
 	var usernam = req.body.username;
 	var date = req.body.datepicker;
-	var db = req.db;
-	var collection = db.get('locations');
+
+	  if (!db) {
+    initDb(function(err){});
+  	}
+
+	var collection = db.collection('locations');
 
 	console.log(usernam);
 
@@ -49,20 +100,20 @@ router.post('/get', function(req,res,next){
 // input data to db
 //
 
-
-
 router.post('/insert', function(req,res,next){
 
-	console.log('hej');
+	console.log('hej', req.db);
     // Set our internal DB variable
-    var db = req.db;
-
+  	if (!db) {
+    initDb(function(err){});
+	}
+	
     // Get our form values. These rely on the "name" attributes
     var userName = req.body.username;
     var locations = req.body.locations;
 
     // Set our collection
-    var collection = db.get('locations');
+    var collection = db.collection('locations');
     console.log('insert');
 
 var user = new Promise(function(resolve,reject){
@@ -152,11 +203,10 @@ user.then(doc =>{
 		        	console.log(doc)
 		            // And forward to success page
 		            res.send(200);
-		        
-    			}
+		        	}
     			});
     		}
-    	})
+    	});
     }
 });
     console.log('koniec');
