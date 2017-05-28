205db4a40816f75a7955ef6962d6c9800700ae60
Czips <mikro121@gmail.com>
2017-05-28 18:07:44 +0200

    Hellow world

---
+++ public/javascripts/user.js
@@ -0,0 +1,29 @@
+<script>
+$(function() {
+  $("#inputDate").datepicker();
+  });
+
+function initMap(){
+  var locations =   JSON.parse(document.getElementById('target').value);
+  console.log(locations);
+  var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 10,
+  center: new google.maps.LatLng(50.048471, 19.947245),
+  mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+  var infowindow = new google.maps.InfoWindow();
+  var marker, i;
+  for (i = 0; i < locations.length; i++) {  
+  marker = new google.maps.Marker({
+  position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+  map: map
+  });
+  google.maps.event.addListener(marker, 'click', (function(marker, i) {
+  return function() {
+  infowindow.setContent(locations[i][0]);
+  infowindow.open(map, marker);
+  }
+  })(marker, i));
+  }
+}
+</script>
---
+++ routes/index.js
@@ -0,0 +1,222 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path')
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //res.sendFile(path.resolve('views/index.html'));
+  res.render('index', { title: 'Express' });
+});
+
+
+router.post('/get', function(req,res,next){
+	console.log('hejj');
+	var usernam = req.body.username;
+	var date = req.body.datepicker;
+	var db = req.db;
+	var collection = db.get('locations');
+
+	console.log(usernam);
+
+
+	collection.find( {"username":usernam},function(err,docs){
+		if(err != null)
+		{
+			//console.log(err);
+			res.sendStatus(404);
+		}else
+		{
+		var arrayLength = docs[0].locations.length;
+		for (var i = 0; i < arrayLength; i++) {
+		    //Do something
+			if(docs[0].locations[i].Date == date){
+				 res.render('index', { 
+			 	"locationlist" : JSON.stringify(docs[0].locations[i].points)
+				 });
+			}
+		}
+
+			res.sendStatus(404);
+		}
+	});
+});
+
+//
+// input data to db
+//
+
+
+
+router.post('/insert', function(req,res,next){
+
+	console.log('hej');
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    var locations = req.body.locations;
+
+    // Set our collection
+    var collection = db.get('locations');
+    console.log('insert');
+
+let user = new Promise(function(resolve,reject){
+    	collection.find(
+    	{username: userName},function(err,doc){
+    		if(err)
+    		{
+    			reject(err);
+    		}else{
+    			resolve(doc);
+    		}
+    	});
+});
+
+
+let findLocations = new Promise( (resolve, reject) =>{
+	collection.find({ "locations.0.Date": {$eq: locations.Date}},
+		function(err,doc){
+			if(err){
+				reject(err);
+			}else{
+				resolve(doc);
+			}
+		}
+		);
+});
+
+
+
+    console.log(user);
+
+user.then(doc =>{
+
+	if(doc.length ==0){
+    	console.log('insert user');
+    	collection.insert({
+    		username: userName,
+    		locations: [ locations]
+    	},
+    		function (err, doc) {
+		        if (err) {
+		        	console.log(err)
+		            // If it failed, return error
+		            res.send(403);
+		        }
+		        else {
+		        	console.log(doc)
+		            // And forward to success page
+		            res.send(200);
+		        }
+		    }
+    	);
+    }
+    else
+    {
+    	findLocations.then(doc =>{
+    		
+    		//day not found
+    		if(doc.length ==0)
+    		{
+    			console.log('add locations');
+    			collection.update({username: userName},
+    			{$addToSet:{locations: locations}},function(err,doc){
+    			if (err) {
+		        	console.log(err)
+		            // If it failed, return error
+		            res.send(403);
+		        }
+		        else {
+		        	console.log(doc)
+		            // And forward to success page
+		            res.send(200);
+		        
+    			}
+    			});
+    		}else
+    		{
+    			console.log('push points');
+    			collection.update({username: userName},
+    			{$pushAll:{ "locations.0.points": locations.points}},function(err,doc){
+    			if (err) {
+		        	console.log(err)
+		            // If it failed, return error
+		            res.send(403);
+		        }
+		        else {
+		        	console.log(doc)
+		            // And forward to success page
+		            res.send(200);
+		        
+    			}
+    			});
+    		}
+    	})
+    }
+});
+// });
+// 	}else
+// 	{
+// 		if(!collection.find(
+// 		{
+// 			_id: locations._id
+// 		}))
+// 		{
+// 			collection.update(
+// 		    { username: userName},
+// 		    {
+// 		        $addToSet: {locations : locations}
+// 		    },{upsert: true}, function (err, doc) {
+// 		        if (err) {
+// 		            // If it failed, return error
+// 		            res.send(403);
+// 		        }
+// 		        else {
+// 		            // And forward to success page
+// 		            res.send(200);
+// 		        }
+// 		    });
+// 		}
+// 		else
+// 		{
+// 			collection.update(
+// 		    { _id: locations._id},
+// 		    {
+// 		        locations : locations
+// 		    },{upsert: true}, function (err, doc) {
+// 		        if (err) {
+// 		            // If it failed, return error
+// 		            res.send(403);
+// 		        }
+// 		        else {
+// 		            // And forward to success page
+// 		            res.send(200);
+// 		        }
+// 		    });
+// 		}
+
+// 	}
+
+
+
+    // Submit to the DB
+    // collection.update(
+    // { username: userName},
+    // {
+    //     $addToSet: {locations : locations}
+    // },{upsert: true}, function (err, doc) {
+    //     if (err) {
+    //         // If it failed, return error
+    //         res.send(403);
+    //     }
+    //     else {
+    //         // And forward to success page
+    //         res.send(200);
+    //     }
+    // });
+
+    console.log('koniec');
+    //res.send(200);
+});
+
+module.exports = router;
